f3d2ce0a91b3a1af7a8e9514b8dcc1d2b59bd675
Noura Ashraf Elsaadany <nouraelsaadany@hotmail.com>
2019-01-31 01:32:20 +0200

    Update newListOnClick.js

--- newListOnClick.js
+++ newListOnClick.js
@@ -1,3 +1,7 @@
 document.getElementById('Plus').addEventListener('click', function() {
-    alert('Please type anything');
+    // alert('Please type anything');
+
+    var UserInput = document.getElementById('form-name').value;
+    console.log(UserInput);
+    alert('You have typed :' + " " + UserInput);
 });
