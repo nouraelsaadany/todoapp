c7b8b4686a733ad9a08f2f36291697596ae63937
Noura Ashraf Elsaadany <nouraelsaadany@hotmail.com>
2019-02-02 13:56:30 +0200

    add

--- newListOnClick.js
+++ newListOnClick.js
@@ -2,6 +2,21 @@ document.getElementById('Plus').addEventListener('click', function() {
     // alert('Please type anything');
 
     var UserInput = document.getElementById('form-name').value;
-    console.log(UserInput);
-    alert('You have typed :' + " " + UserInput);
+    // console.log(UserInput);
+    //alert('You have typed :' + " " + UserInput);
+
+    var OuterDiv = document.createElement('div');
+    OuterDiv.id = "outerDiv";
+    OuterDiv.className = "flex-container2 row";
+
+    var node = document.createTextNode(UserInput);
+    OuterDiv.appendChild(node);
+
+    var element = document.getElementById("wrap");
+    element.appendChild(OuterDiv);
+    //element.insertBefore(OuterDiv, element);
+    console.log(OuterDiv.className)
+
 });
+
+// Creating new div
