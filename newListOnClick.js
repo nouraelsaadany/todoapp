7a41da047f3249c5c9bef5d97b6d06bbb0b0c177
Noura Ashraf Elsaadany <nouraelsaadany@hotmail.com>
2019-02-03 01:09:40 +0200

    final result
    all functionallities are working except the delete button

--- newListOnClick.js
+++ newListOnClick.js
@@ -1,3 +1,4 @@
+var counter = 1;
 document.getElementById('Plus').addEventListener('click', function() {
     // alert('Please type anything');
 
@@ -5,17 +6,83 @@ document.getElementById('Plus').addEventListener('click', function() {
     // console.log(UserInput);
     //alert('You have typed :' + " " + UserInput);
 
+    //THE VERY OUTER DIV
     var OuterDiv = document.createElement('div');
     OuterDiv.id = "outerDiv";
+    // Div for the left side
+    var LeftDiv = document.createElement('div');
+    // div for the left side for X
+    var LeftDivX = document.createElement('div');
+    // div for the listed items
+    var ListedItemsDiv = document.createElement('div');
+    //div for deleting
+    var DeletingListButtonDiv = document.createElement('div');
+    //  THE OVERALL DIV
+    var OverAllDiv = document.getElementById("wrap");
+
+    // sub divs
+    var LeftSubDiv = document.createElement('div');
+    var LeftDivX = document.createElement('div');
+    var LeftDivBtnX = document.createElement('div');
+    // Setting Classes to div
+
     OuterDiv.className = "flex-container2 row";
+    LeftDiv.className = "height-width50 Right-List-backGround column-2";
+    LeftDivX.className = "Right-List-backGround  column-1";
+    ListedItemsDiv.className = "EnterNewTODO column-8";
+    DeletingListButtonDiv.className = "EnterNewTODO column-1";
 
-    var node = document.createTextNode(UserInput);
-    OuterDiv.appendChild(node);
 
-    var element = document.getElementById("wrap");
-    element.appendChild(OuterDiv);
+
+
+    //Style
+    LeftSubDiv.style = "justify-content: space-between";
+
+    //For Creating new List this is all what we need 
+    var LeftSubDivText = document.createElement('p');
+    LeftSubDivText.style = "color: white; justify-items: center";
+    LeftDivBtnX.className = "theXPosition";
+    var LeftXBtn = document.createElement("button");
+    LeftXBtn.style = "background: rgb(151, 80, 92)";
+    LeftXBtn.appendChild(document.createTextNode("X"));
+
+    //Adding new divs to the whole outer div
+    OverAllDiv.append(OuterDiv);
+
+
+    // appending/ creating the whole row with the 4 columns
+    OuterDiv.append(LeftDiv);
+    OuterDiv.append(LeftDivX);
+    OuterDiv.append(ListedItemsDiv);
+    OuterDiv.append(DeletingListButtonDiv);
+
+    // left div creation 
+    LeftDiv.append(LeftSubDiv);
+    LeftSubDiv.append(LeftSubDivText);
+    LeftSubDivText.appendChild(document.createTextNode("List Number :" + "" + counter++));
+    LeftDivX.append(LeftDivBtnX);
+    LeftDivBtnX.append(LeftXBtn);
+
+
+    // right div/ listed items //
+
+    // the user input inserted to the div
+
+    ListedItemsDiv.appendChild(document.createTextNode(UserInput));
+
+    // the deleting button 
+    var DeletingListButtonClass = document.createElement("div");
+    DeletingListButtonClass.className = "theXPosition";
+    var DeletingListButton = document.createElement("button");
+    DeletingListButton.style = "background: rgb(221, 180, 187)"
+
+    DeletingListButtonDiv.append(DeletingListButtonClass);
+    DeletingListButtonClass.append(DeletingListButton);
+
+    DeletingListButton.append(document.createTextNode("   X   "));
     //element.insertBefore(OuterDiv, element);
-    console.log(OuterDiv.className)
+    //console.log(DeletingListButton.className);
+
 
 });
 
--- userInput.js
+++ userInput.js
@@ -1,1 +1,21 @@
-var UserInput = document.getElementById("form-name").value;
+// //var newDiv = document.createElement('div');
+// var addHere = document.getElementById('divToHide1');
+
+// newDiv.className == "flex-container row";
+
+// var TEST = document.getElementById('Test');
+
+// TEST.addEventListener('mouseenter', runevent);
+
+// function runevent() {
+//     console.log('presse');
+// }
+
+
+document.getElementById('Test').addEventListener('click', function() {
+    // alert('Please type anything');
+
+    var UserInput = document.getElementById('form-name').value;
+    console.log(UserInput);
+    alert('You have typed :' + " " + UserInput);
+});
